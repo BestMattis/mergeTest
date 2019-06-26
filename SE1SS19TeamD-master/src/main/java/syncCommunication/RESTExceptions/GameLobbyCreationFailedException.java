742a73f6f98937fbce34762d7def94ef0f5b3633
BestM <opfgmfk@gmail.com>
2019-06-26 16:16:28 +0200

    purge

--- SE1SS19TeamD-master/src/main/java/syncCommunication/RESTExceptions/GameLobbyCreationFailedException.java
+++
@@ -1,8 +0,0 @@
-package syncCommunication.RESTExceptions;
-
-@SuppressWarnings("serial")
-public class GameLobbyCreationFailedException extends RuntimeException {
-    public GameLobbyCreationFailedException(String message) {
-        super(message);
-    }
-}
