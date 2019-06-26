bc04bae0d5a21a6b1fc9689ac47677f9675a4d4c
BestM <opfgmfk@gmail.com>
2019-06-26 16:18:35 +0200

    purge

--- SE1SS19TeamD-master/src/main/java/syncCommunication/RESTExceptions/RegistrationFailedException.java
+++
@@ -1,8 +0,0 @@
-package syncCommunication.RESTExceptions;
-
-@SuppressWarnings("serial")
-public class RegistrationFailedException extends Exception {
-    public RegistrationFailedException(String message) {
-        super(message);
-    }
-}
--- SE1SS19TeamD-master/src/main/java/syncCommunication/RESTExceptions/TooManyRequestsPerSecondException.java
+++
@@ -1,8 +0,0 @@
-package syncCommunication.RESTExceptions;
-
-@SuppressWarnings("serial")
-public class TooManyRequestsPerSecondException extends Exception {
-    public TooManyRequestsPerSecondException(String message) {
-        super(message);
-    }
-}
